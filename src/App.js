9dab1618f011f42b303d20d0197670bde836f6a8
Chloe Harris <chloe.a.harris17@gmail.com>
2023-06-13 13:45:02 -0700

    Basic page layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <nav className="nav-links">
+          <button>Work</button>
+          <button>About</button>
+          <button>Contact</button>
+          <button>Resume</button>
+        </nav>
       </header>
+      <main>
+        <section className="work" alt="work">
+          <div className="container">
+            <div className="work-wrapper">
+              <h1>Work</h1>
+              <div className="grid"></div>
+              <h1>UX Design</h1>
+              <div className="grid"></div>
+            </div>
+          </div>
+        </section>
+        <section className="about-me" alt="about me"></section>
+        <section className="skills" alt="skills"></section>
+        <section className="testimonial" alt="testimonial"></section>
+        <section className="contact" alt="contact"></section>
+      </main>
+      <footer>Made with ❤️ by Chloe Harris</footer>
     </div>
   );
 }
